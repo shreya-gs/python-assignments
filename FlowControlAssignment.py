561c82f9ece0b290adfe94370fc6bdadcae4a5ff
Shreya G S <124378144+shreya-gs@users.noreply.github.com>
2023-02-25 05:54:21 +0000

    Flow control assignment completed

---
+++ FlowControlAssignment.py
@@ -0,0 +1,19 @@
+# greatest of three numbers
+
+num = [10,15,20]
+
+if (num[0] >= num[1]) and (num[0] >= num[2]):
+    print(f"{num[0]} is greatest number.")    
+elif (num[1]>=num[0]) and (num[1]>=num[2]):
+    print(f"{num[1]} is greatest number.")    
+else:
+    print(f"{num[2]} is greatest number.")
+
+# WAP TO CONVERT DIGITS FROM 0-9 ONLY INTO WORDS.
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+s = ''
+for i in numbers:
+    s += str(i)
+    
+print(s, type(s))
