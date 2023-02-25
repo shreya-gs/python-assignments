612bb5d49354fabfdc1cbc5ea1b5d121b2c95a46
Shreya G S <124378144+shreya-gs@users.noreply.github.com>
2023-02-25 06:41:35 +0000

    slicing of string

---
+++ StringSlice.py
@@ -0,0 +1,14 @@
+# WAP TO ACCESS THE STRING USING SLICING AND INDEXING.
+
+# using slicing 
+s = "My name is Shreya."
+a = s[3:7]
+b = s[0:17:2]
+c = s[-7:-1]
+# d = s[-7:0] ==> empty string
+# e = s[-1:-7] ==> empty string
+f = s[-2:-7:-1]
+print(a)
+print(b)
+print(c)
+print(f)
