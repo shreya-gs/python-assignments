677a8e7d8b8b7431559b3843842c8263c6ed922e
Shreya G S <124378144+shreya-gs@users.noreply.github.com>
2023-02-26 10:27:04 +0530

    Update StringSlice.py

--- StringSlice.py
+++ StringSlice.py
@@ -2,6 +2,8 @@
 
 # using slicing 
 s = "My name is Shreya."
+
+# slicing method
 a = s[3:7]
 b = s[0:17:2]
 c = s[-7:-1]
@@ -12,3 +14,12 @@ print(a)
 print(b)
 print(c)
 print(f)
+
+s = "My name is Shreya."
+# indexing method
+i = 0
+while i < (len(s)/2):
+  print(s[i], end = '')
+  
+# to get single character
+print(s[5])
